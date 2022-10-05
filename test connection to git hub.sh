7f1f507d020a2ff9c0538ced640bd7c89f5b77e0
laraparata <110800070+laraparata@users.noreply.github.com>
2022-10-05 16:32:09 +0800

    comments

--- test connection to git hub.sh
+++ test connection to git hub.sh
@@ -1,2 +1,11 @@
 #!/bin/bash 
 
+#^ a line you always need at the start to tell R we're using bash / shell commands 
+
+echo "Test connection to github in R project" >> README.md 
+
+git add . ##add any changes that have been made in the current directory to the list of files being committed / pushed
+
+git commit -m "A commit from my R project on my local computer" #the update 
+
+git push # the command to actually push the changes to github 
