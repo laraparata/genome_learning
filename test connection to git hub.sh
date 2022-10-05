ea536cadcb67f9c99f00bea8d649153b867d2712
laraparata <110800070+laraparata@users.noreply.github.com>
2022-10-05 16:32:49 +0800

    comments

--- test connection to git hub.sh
+++ test connection to git hub.sh
@@ -4,8 +4,10 @@
 
 echo "Test connection to github in R project" >> README.md 
 
-git add . ##add any changes that have been made in the current directory to the list of files being committed / pushed
+git add "test connection to git hub.sh" #if you want to select only 1 file to save changes to 
 
-git commit -m "A commit from my R project on my local computer" #the update 
+git add . #add any changes that have been made in the current directory to the list of files being committed / pushed
+
+git commit -m "comments" #the update 
 
 git push # the command to actually push the changes to github 
